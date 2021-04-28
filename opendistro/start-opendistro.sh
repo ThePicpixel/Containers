23c5056cf4e19df666789e768e38d7c34f7e9b40
Hugo PICHON <thepicpixel@r0nd.fr>
2021-04-28 15:29:08 +0200

    Prevent script from printing output in stdout

--- opendistro/start-opendistro.sh
+++ opendistro/start-opendistro.sh
@@ -2,13 +2,12 @@
 
 # Creates a pod named opendistro for our deployement
 echo "[*] Creating pod opendistro..."
-podman pod create --name opendistro --publish 5601:5601 --publish 9200:9200 
+podman pod create --name opendistro --publish 5601:5601 --publish 9200:9200 --publish 5140:5140
 
 # Deploys our elastic search container
 echo "[*] Running opendistro.elastic into pod opendistro"
-podman run --pod opendistro --name opendistro.elastic -e "discovery.type=single-node" amazon/opendistro-for-elasticsearch &
+podman run --pod opendistro --name opendistro.elastic -d -e "discovery.type=single-node" amazon/opendistro-for-elasticsearch
 
 # Deploys our kibana container
 echo "[*] Running opendistro.kibana into pod opendistro"
-podman run --pod opendistro --name opendistro.kibana -e "ELASTICSEARCH_URL=https://localhost:9200" -e "ELASTICSEARCH_HOSTS=https://localhost:9200" amazon/opendistro-for-elasticsearch-kibana &
-
+podman run --pod opendistro --name opendistro.kibana -d -e "ELASTICSEARCH_URL=https://localhost:9200" -e "ELASTICSEARCH_HOSTS=https://localhost:9200" amazon/opendistro-for-elasticsearch-kibana
